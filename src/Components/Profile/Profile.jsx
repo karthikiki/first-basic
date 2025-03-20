e44f6bc60189cde2a50fb6d60f6d3a27ce9b15dc
karthikiki <karthickcks003@gmail.com>
2025-03-20 23:53:56 +0530

    first

---
+++ src/Components/Profile/Profile.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Profile.css'
+import profile_icon from '../Asserts/base.png'
+
+const Profile = () => {
+  return (
+    <div className='userProfile'>
+       <div className='gradiant'></div>
+       <div className="profile-name"></div>
+       <img src={profile_icon} alt=''></img>
+       <div className="profile title">Karthick C</div>
+       <div className="profile description">
+        MERN-Stack Developer // Freelancer
+        </div>
+        <div className="profile-button"><a href='mailto.karthick@gmail.com'>Contact Me</a></div>  
+    </div>
+  )
+}
+
+export default Profile
